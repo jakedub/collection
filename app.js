7ea474f9491a401646259da03452816c4c81fb19
jakedub <jacob.w.moore@gmail.com>
2017-08-23 16:40:38 -0400

    cannot get it to render to completed, edit, or delete

--- app.js
+++ app.js
@@ -1,48 +1,26 @@
-//Mongo
-const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017/comic";
-
-//body parser
-const bodyParser = require("body-parser");
-// app.use(bodyParser.urlencoded({extended:true}));
-
-
-const Book = require("./models/comic");
-
 //Express
 const express = require ("express");
 const app = express ();
-
+//Mongo
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/comic";
+//body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+//model
+const Book = require("./models/comic");
 //Mustache
 const mustacheExpress = require("mustache-express");
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views")
 app.set("view engine", "mustache")
-
 //styling
 app.use(express.static("public"))
-
 //Mongoose
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/comic');
 
-// MongoClient.connect(url)
-//   .then(function(db){
-//     return db.collection("comic").insertMany(data.book)
-//   })
-//   .then(function(result){
-//     console.log(result);
-//   });
-
-// let newName = new Book ({name:"Journey into Mystery"});
-// newName.save()
-// .then(function(){
-//   db.group.insertOne(newName);
-// })
-// .catch(function(){
-//   // console.log("not valid");
-// })
 function handleSuccess(result){
   console.log(result);
 }
@@ -56,11 +34,6 @@ app.get('/', function (req, res) {
   res.render("index");
 });
 
-//rendering the return
-app.get("/completed", function (req,res){
-  res.render("completed");
-});
-
 //unsaved model that's being saved
 // Book.create({name: "Amazing Spider-Man"})
 //   .then(handleSuccess)
@@ -72,68 +45,53 @@ app.get("/completed", function (req,res){
 
 //rendering to the completed page
 
-
-
-let list =  [{
-  name: "Superman",
-  source: [{
-    online: true,
-    print: false
-  }],
-  version: [{
-    issue: 619,
-    print: 01/01/2001,
-    volume: 1,
-    writer: "Kurt Busiek",
-    artist: "Alex Ross",
-    characters: [{
-      good: "Superman, Lois Lane",
-      bad: "Lex Luthor",
-      firstAppearance: true
+app.post("/", function(req,res){
+  console.log(req.body);
+  Book.create({
+    name: req.body.name,
+    source: [{
+      online: req.body.online,
+      print: req.body.print
+    }],
+    version: [{
+      issue: req.body.issue,
+      print: req.body.print,
+      volume: req.body.volume,
+      writer: req.body.writer,
+      artist: req.body.artist,
+      characters: [{
+        good: req.body.good,
+        bad: req.body.bad,
+        firstAppearance: req.body.firstAppearance
+      }]
+    }],
+    gradient: [{
+      grade: req.body.grade,
+      checker: req.body.checker,
+      comments: req.body.comments,
+      signatures: [{
+        artist: req.body.artist,
+        writer: req.body.writer
+      }]
     }]
-  }],
-  gradient: [{
-    grade: 9,
-    checker: "Bill Thomas",
-    comments: "This is a comment",
-    signatures: [{
-      artist: true,
-      writer: true
-    }]
-  }]
-}]
+  })
+  .then(handleSuccess)
+  .catch(handleError);
 
-// Book.create(list)
-// .then(handleSuccess)
-// .catch(handleError);
-
-const data = {
-  bookList:list
-};
+  res.redirect("/completed")
+});
 
 app.get("/completed", function(req, res){
   MongoClient.connect(url)
   .then(function(db){
     return db.collection("books").find().toArray(function(err, doc){
+      console.log(docs);
       res.render("completed", {data:doc});
     });
     db.close();
   });
 });
 
-app.post("/", function(req,res){
-  list.push({bookList: data});
-  res.redirect("/")
-});
-
-app.post("/completed", function(req,res){
-  console.log(req.body);
-  let completed =req.body.marked;
-  function findItems(item){
-    return item.todo ===completed;}
-    list.find(findItems);
-    res.redirect("/");
-  });
 
   app.listen(3000, function () {
     console.log('Wolverines!!!');
