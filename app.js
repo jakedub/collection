044864ed85847d239c6a922d530552fbd533a9f1
jakedub <jacob.w.moore@gmail.com>
2017-08-29 15:31:29 -0400

    closer I think...?

--- app.js
+++ app.js
@@ -34,6 +34,57 @@ app.get('/', function (req, res) {
   res.render("index");
 });
 
+
+//render the edit page. Needs to pass through the object from Mongo
+app.get("/edit/:name", function(req, res) {
+  MongoClient.connect(url, function(db) {
+    db.collections("books").findOne({name: req.params.name}).then(function(docs) {
+      console.log(docs);
+      res.render("edit", docs);
+    });
+    db.close();
+  })
+});
+
+app.post("/edit/:name/edit", function(req, res) {
+  MongoClient.connect(url, function(err, db) {
+    console.log("Edit the Collection");
+    Book.updateOne({name: req.params.name}, {$set: {
+      name: req.body.name,
+      source: [{
+        online: req.body.online,
+        print: req.body.print
+      }],
+      version: [{
+        issue: req.body.issue,
+        print: req.body.print,
+        volume: req.body.volume,
+        writer: req.body.writer,
+        artist: req.body.artist,
+        characters: [{
+          good: req.body.good,
+          bad: req.body.bad,
+          firstAppearance: req.body.firstAppearance
+        }]
+      }],
+      gradient: [{
+        grade: req.body.grade,
+        checker: req.body.checker,
+        comments: req.body.comments,
+        signatures: [{
+          artist: req.body.artist,
+          writer: req.body.writer
+        }]
+      }]
+      {$push: {[version: issue]: req.body.issue}}});
+    .then(function() {
+      console.log("Present");
+      res.redirect("/completed");
+      db.close();
+    })
+  })
+});
+
 //unsaved model that's being saved
 // Book.create({name: "Amazing Spider-Man"})
 //   .then(handleSuccess)
@@ -93,44 +144,73 @@ app.get("/completed", function(req, res){
     // db.close();
   });
 
-  app.post("/edit", function(req, res) {
-    MongoClient.connect(url, function(err, db) {
-      console.log("Edit the Collection");
-      Book.updateOne({name: req.params.name}, {$set: {
-        name: req.body.name,
-        source: [{
-          online: req.body.online,
-          print: req.body.print
-        }],
-        version: [{
-          issue: req.body.issue,
-          print: req.body.print,
-          volume: req.body.volume,
-          writer: req.body.writer,
-          artist: req.body.artist,
-          characters: [{
-            good: req.body.good,
-            bad: req.body.bad,
-            firstAppearance: req.body.firstAppearance
-          }]
-        }],
-        gradient: [{
-          grade: req.body.grade,
-          checker: req.body.checker,
-          comments: req.body.comments,
-          signatures: [{
-            artist: req.body.artist,
-            writer: req.body.writer
-          }]
-        }]
-        {$push: {issue: req.body.issue}}});
-      .then(function() {
-        console.log("Present");
-        res.redirect("/completed");
-        db.close();
-      });
-    });
-  });
+//editing issues
+  // app.post("/edit", function(req, res) {
+  //   MongoClient.connect(url, function(err, db) {
+  //     console.log("Edit the Collection");
+  //     Book.updateOne({name: req.params.name}, {$set: {
+  //       name: req.body.name,
+  //       source: [{
+  //         online: req.body.online,
+  //         print: req.body.print
+  //       }],
+  //       version: [{
+  //         issue: req.body.issue,
+  //         print: req.body.print,
+  //         volume: req.body.volume,
+  //         writer: req.body.writer,
+  //         artist: req.body.artist,
+  //         characters: [{
+  //           good: req.body.good,
+  //           bad: req.body.bad,
+  //           firstAppearance: req.body.firstAppearance
+  //         }]
+  //       }],
+  //       gradient: [{
+  //         grade: req.body.grade,
+  //         checker: req.body.checker,
+  //         comments: req.body.comments,
+  //         signatures: [{
+  //           artist: req.body.artist,
+  //           writer: req.body.writer
+  //         }]
+  //       }]
+  //       {$push: {issue: req.body.issue}}});
+  //     .then(function() {
+  //       console.log("Present");
+  //       res.redirect("/completed");
+  //       db.close();
+  //     });
+  //   });
+  // });
+
+//creates a hashed password. that being the password set for all the robots
+app.get("/", function(req, res){
+  let hash = bcrypt.hashSync("that", 8)
+  console.log(hash);
+})
+
+app.get("/login", function(req,res){
+  res.render("login");
+});
+
+app.post("/login", function(req,res){
+  let username = req.body.username
+  let password = req.body.password
+  MongoClient.connect(url)
+  .then(function functionName(db){
+    db.collection("users")
+    .findOne({username: username})
+    .then(function(data){
+      db.close()
+      if (bcrypt.compareSynce(password.data.passwordHash)){
+        res.render("/");
+      } else {
+        res.send("no")
+      }
+    })
+  })
+})
 
   app.listen(3000, function () {
     console.log('Wolverines!!!');
